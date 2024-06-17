c11ea0ac50ac93e1733461f764f1690ce36f233a
saumya-chaturvedi <schaturv@mpi-sws.org>
2024-06-17 10:37:38 +0530

    syntax error

--- lora_context_finetuning.py
+++ lora_context_finetuning.py
@@ -35,7 +35,7 @@ def print_trainable_parameters(model):
     print(
         f"trainable params: {trainable_params} || all params: {all_param} || trainable%: {100 * trainable_params / all_param}"
     )
-(* 
+  
 """#### Obtain LoRA Model"""
 # HuggingFace's Inbuilt Lora implementation
  

7fbf61a934a9fdcc0619c13e471096594ba44595
saumya-chaturvedi <schaturv@mpi-sws.org>
2024-06-18 18:54:02 +0530

    changed dataset variable name

--- CALM-experiments/llama_lora_finetune_on_mappings.py
+++ CALM-experiments/llama_lora_finetune_on_mappings.py
@@ -183,7 +183,7 @@ import transformers
 
 trainer = transformers.Trainer(
     model=model,
-    train_dataset=mapped_qa_dataset["train"],
+    train_dataset=dataset["text"],
     args=transformers.TrainingArguments( 
         per_device_train_batch_size=4,
         gradient_accumulation_steps=4,

7888b0126d46d1e39a1d763ed548361717236724
saumya-chaturvedi <schaturv@mpi-sws.org>
2024-06-20 18:05:34 +0530

    added code to generate prompts and load weighted adapters together

--- CALM-experiments/merged_adapter_inference.py
+++ CALM-experiments/merged_adapter_inference.py
@@ -8,9 +8,41 @@ compute_dtype = getattr(torch, "float16")
 model = AutoModelForCausalLM.from_pretrained(
         base_model, device_map={"": 0})
 
+tokenizer = AutoTokenizer.from_pretrained(base_model, use_fast=True)
+
 model = PeftModel.from_pretrained(model, "schaturv/llama2-7b-arithmetic-calculations-adapter", adapter_name="arithmetic")
 
 model.load_adapter("schaturv/llama2-7b-key-value-adapter", adapter_name="pairings")
 
 print(model)
+
+# combining adapters using cat
+model.add_weighted_adapter(["arithmetic", "pairings"], [1.0,1.0], combination_type="cat", adapter_name="pairings_arithmetic")
+
+# remove the single adapters
+model.delete_adapter("arithmetic")
+model.delete_adapter("pairings")
+model.save_pretrained("./pairings_arithmetic")
+
+model = PeftModel.from_pretrained(model, "./pairings_arithmetic/")
+
+# prompt generating function
+def generate(prompt):
+  tokenized_input = tokenizer(prompt, return_tensors="pt")
+  input_ids = tokenized_input["input_ids"].cuda()
+
+  generation_output = model.generate(
+          input_ids=input_ids,
+          num_beams=1,
+          return_dict_in_generate=True,
+          output_scores=True,
+          max_new_tokens=130
+
+  )
+  for seq in generation_output.sequences:
+      output = tokenizer.decode(seq, skip_special_tokens=True)
+      print(output.strip())
+
+
+
 

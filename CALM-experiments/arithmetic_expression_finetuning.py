3e8e955636e5b2ff5989043102251d298ac6d063
saumya-chaturvedi <schaturv@mpi-sws.org>
2024-06-26 16:15:03 +0530

    sending model to device

--- CALM-experiments/arithmetic_expression_finetuning.py
+++ CALM-experiments/arithmetic_expression_finetuning.py
@@ -202,10 +202,12 @@ trainer = Trainer(
 
 print("active adapter before training: ", model.active_adapters)
 
+model.to(device)
+
 # testing on one prompt
 prompt = "Perform the arithmetic calculation to get the answer.\n\n### Arithmetic Expression:\n'24 - 61'\n\n### Answer:"
 inputs = tokenizer(prompt, return_tensors="pt").input_ids
-outputs = model.generate(inputs, max_new_tokens=100, do_sample=True, top_k=50, top_p=0.95).to(device)
+outputs = model.generate(inputs, max_new_tokens=100, do_sample=True, top_k=50, top_p=0.95)
 tokenized_output = tokenizer.batch_decode(outputs, skip_special_tokens=True)
 print(tokenized_output)
 

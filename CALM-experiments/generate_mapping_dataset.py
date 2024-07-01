90816b12d4c039e74e0ab68e6b7d2535d340a0f7
saumya-chaturvedi <schaturv@mpi-sws.org>
2024-07-01 17:50:02 +0530

    rectified the code for generating dataset for arithmetic training for 2nd adapter

--- CALM-experiments/generate_mapping_dataset.py
+++ CALM-experiments/generate_mapping_dataset.py
@@ -7,6 +7,7 @@ import json
 
 LEN_DATASET = 48000
 RANGE_OF_MAPPINGS = 100
+OPERATORS = [' + ', ' - ']
 
 def generate_string_to_number_mappings(count):
     unique_values = list(range(1, count + 1))
@@ -22,34 +23,42 @@ def generate_string_to_number_mappings(count):
     return knowledge_artifact
 
 knowledge_artifact = generate_string_to_number_mappings(RANGE_OF_MAPPINGS)
-# print(knowledge_artifact)
-
-# realized a namedtuple is not needed here
-# arithmetic_expression = namedtuple("expression_properties", "key_expression value_expression value")
 
 def generate_random_key():
     return choice(list(knowledge_artifact.keys()))
 
-def create_arithmetic_expressions():
-    arithmetic_key_expression = ''
+def create_numeric_arithmetic_expressions():
     arithmetic_value_expression = ''
 
     for _ in range(choice(list(range(1, 5)))):
-        operator = choice([' + ', ' - '])
+        operator = choice(OPERATORS)
         key = generate_random_key()
-        arithmetic_key_expression += key + operator
         arithmetic_value_expression += str(knowledge_artifact[key]) + operator
 
     last_key = generate_random_key()
-    arithmetic_key_expression += last_key
     arithmetic_value_expression += str(knowledge_artifact[last_key])
     arithmetic_value = eval(arithmetic_value_expression)    
 
-    return arithmetic_key_expression, arithmetic_value_expression, arithmetic_value
+    return arithmetic_value_expression, arithmetic_value
+
+def generate_arithmetic_training_dataset(length):
+    arithmetic_value_expressions = []
+    arithmetic_values = []
+
+    for _ in range(length):
+        arithmetic_value_expression, arithmetic_value = create_numeric_arithmetic_expressions()
+        arithmetic_value_expressions.append(arithmetic_value_expression)
+        arithmetic_values.append(arithmetic_value)
+    
+    # print(arithmetic_key_expressions, arithmetic_value_expressions, arithmetic_values)
+
+    val_expr_to_arithmetic_val = list(zip(arithmetic_value_expressions, arithmetic_values))
+
+    return val_expr_to_arithmetic_val
+
+val_expr_to_arithmetic_val = generate_arithmetic_training_dataset(LEN_DATASET)
 
 arithmetic_key_expressions = []
-arithmetic_value_expressions = []
-arithmetic_values = []
 
 def create_arithmetic_expressions_from_keys(map):
     keys = list(map.keys())
@@ -121,44 +130,27 @@ def generate_merged_dataset(size):
 
 print(generate_merged_dataset(5))
 
-def generate_arithmetic_training_dataset(length):
-
-    for _ in range(length):
-        arithmetic_key_expression, arithmetic_value_expression, arithmetic_value = create_arithmetic_expressions()
-        arithmetic_value_expressions.append(arithmetic_value_expression)
-        arithmetic_values.append(arithmetic_value)
-    
-    # print(arithmetic_key_expressions, arithmetic_value_expressions, arithmetic_values)
-
-    key_expr_to_val_expr = list(zip(arithmet    ic_key_expressions, arithmetic_value_expressions))
-    key_expr_to_arithmetic_val = list(zip(arithmetic_key_expressions, arithmetic_values))
-    val_expr_to_arithmetic_val = list(zip(arithmetic_value_expressions, arithmetic_values))
-
-    return key_expr_to_val_expr, key_expr_to_arithmetic_val, val_expr_to_arithmetic_val
-
-key_expr_to_val_expr, key_expr_to_arithmetic_val, val_expr_to_arithmetic_val = generate_datasets(LEN_DATASET)
-
 # METHOD 1 to create datasets:
 D_KV_SUBS, D_KV_VAL, D_SUBS_VAL =  [], [], []
 
-def create_dataset_list(mapping, dataset_list):
+def create_arithmetic_dataset_list(mapping, dataset_list):
     for key, val in mapping:
         record = defaultdict()
         record["key"] = key
         record["value"] = val
         dataset_list.append(record)
 
-create_dataset_list(val_expr_to_arithmetic_val, D_SUBS_VAL)
+create_arithmetic_dataset_list(val_expr_to_arithmetic_val, D_SUBS_VAL)
 
-f = open("test_samples_key_value_pairings.txt", "w")
-prompt, ans = key_expr_to_val_expr[3]
-f.write(prompt+"\n"+ans)
-f.close()
+# f = open("test_samples_key_value_pairings.txt", "w")
+# prompt, ans = key_expr_to_val_expr[3]
+# f.write(prompt+"\n"+ans)
+# f.close()
 
-f = open("test_samples_key_solution_pairings.txt", "w")
-prompt, ans = key_expr_to_arithmetic_val[3]
-f.write(prompt+"\n"+str(ans))
-f.close()
+# f = open("test_samples_key_solution_pairings.txt", "w")
+# prompt, ans = key_expr_to_arithmetic_val[3]
+# f.write(prompt+"\n"+str(ans))
+# f.close()
 
 # adapter 1
 with open("D_KV_SUBS.json", "w") as outfile:

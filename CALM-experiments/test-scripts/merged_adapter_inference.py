02adc6e87ea212d430306cd05666264f07638f59
saumya-chaturvedi <schaturv@mpi-sws.org>
2024-07-01 21:41:48 +0530

    trying to run inference on merged adapter in separate file

--- CALM-experiments/test-scripts/merged_adapter_inference.py
+++ CALM-experiments/test-scripts/merged_adapter_inference.py
@@ -25,39 +25,17 @@ print("Active adapters: ", peft_model.active_adapters)
 
 # breakpoint()
 
-# config = PeftConfig.from_pretrained("schaturv/pairings_arithmetic")
-# model = AutoModelForCausalLM.from_pretrained(config.base_model_name_or_path)
-# lora_model = PeftModel.from_pretrained(model, "schaturv/pairings_arithmetic")
+# testing on prompts
+outfile = open("merged_adapter_inference_outputs.txt", 'w')
+with open("inference_for_merged_adapter.txt") as file:
+    prompts = [line.rstrip() for line in file]
 
-# model.config.to_json_file("adapter_config.json")
-# model.push_to_hub("schaturv/pairings_arithmetic")
+for prompt in prompts:
+    inputs = tokenizer(prompt, return_tensors="pt").input_ids
+    inputs = inputs.to('cuda')
+    outputs = model.generate(inputs, max_new_tokens=100, do_sample=True, top_k=50, top_p=0.95)
+    tokenized_output = tokenizer.batch_decode(outputs, skip_special_tokens=True)
+    outfile.write(tokenized_output)
+    print(tokenized_output)
 
-# model = PeftModel.from_pretrained(model)
-# print("active adapters: ", model.active_adapters)
-# print("active adapters: ", model.active_adapter)
-
-# prompt generating function
-def generate(prompt):
-  tokenized_input = tokenizer(prompt, return_tensors="pt")
-  print(tokenized_input)
-  input_ids = tokenized_input["input_ids"].cuda()
-
-  generation_output = peft_model.generate(
-          input_ids=input_ids,
-          num_beams=1,
-          return_dict_in_generate=True,
-          output_scores=True,
-          max_new_tokens=200
-
-  )
-  for seq in generation_output.sequences:
-      output = tokenizer.decode(seq, skip_special_tokens=True)
-      print(output.strip())
-
-# peft model not suitable for 
-# pipe = pipeline(task="text-generation", model=model, tokenizer=tokenizer, max_length=200)
-# result = pipe("Find the computation of this arithmetic expression: 22032 * 17024 - 19049 + 5049 + 21763")
-# print(result[0]['generated_text'])
-
-print(generate("Perform the arithmetic calculations to get the desired solution.\n\n"
-            "### Key:22032 * 17024 - 19049 + 5049 + 21763\n\n### Value:\n</s>"))
+outfile.close()

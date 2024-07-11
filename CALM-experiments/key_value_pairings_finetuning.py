ddc035e5ca354727ff43192faab705e9ac42f086
saumya-chaturvedi <schaturv@mpi-sws.org>
2024-07-11 22:55:18 +0530

    epochs -> 500

--- CALM-experiments/key_value_pairings_finetuning.py
+++ CALM-experiments/key_value_pairings_finetuning.py
@@ -119,7 +119,7 @@ from trl import SFTTrainer
 
 batch_size = 4
 gradient_accumulation_steps = 4
-num_train_epochs = 1000
+num_train_epochs = 500
 
 total_num_steps = num_train_epochs * total_sequences // (batch_size * gradient_accumulation_steps)
 

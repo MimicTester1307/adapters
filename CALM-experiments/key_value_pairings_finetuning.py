704e10587fa05a04177502d58b4dfecaf8b1f45a
saumya-chaturvedi <schaturv@mpi-sws.org>
2024-07-19 16:52:41 +0530

    eval steps to be integer, not float

--- CALM-experiments/key_value_pairings_finetuning.py
+++ CALM-experiments/key_value_pairings_finetuning.py
@@ -144,7 +144,7 @@ training_args = TrainingArguments(
     gradient_checkpointing=True,
     gradient_checkpointing_kwargs=dict(use_reentrant=False),
     evaluation_strategy="steps",
-    eval_steps=(len(eval_ds_packed) / batch_size),
+    eval_steps=(len(eval_ds_packed) // batch_size),
     # logging strategies
     logging_strategy="steps",
     logging_steps=1,
